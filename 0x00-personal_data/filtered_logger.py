cfe3bf3eec358ec2edc50349729bb80026c8a083
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-30 16:47:45 +0100

    commit changes

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,87 +1,124 @@
 #!/usr/bin/env python3
 """
-Personal data
+This module provides a function to obfuscate specified fields in a log message,
+a RedactingFormatter class to format log records with obfuscated values,
+and a get_logger function to configure a logger with specific settings.
 """
 
-
-import logging
 import os
 import re
-from typing import List
+from os import environ
+import logging
+from typing import List, Tuple
 import mysql.connector
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Obfuscate specified fields in the log message.
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """ Replacing """
-    for f in fields:
-        message = re.sub(rf"{f}=(.*?)\{separator}",
-                         f'{f}={redaction}{separator}', message)
-    return message
+    :param fields: List of strings representing fields to obfuscate.
+    :param redaction: String representing the replacement text.
+    :param message: The log message.
+    :param separator: The field separator in the log message.
+    :return: The obfuscated log message.
+    """
+    pattern = '|'.join(f'(?<={field}=)[^{separator}]+' for field in fields)
+    return re.sub(pattern, redaction, message)
 
 
 class RedactingFormatter(logging.Formatter):
-    """ RedactingFormatter class. """
+    """
+    Redacting Formatter class for filtering sensitive information from logs.
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """ Init """
-        self.fields = fields
+        """
+        Initialize the formatter with fields to redact.
+
+        :param fields: List of strings representing fields to obfuscate.
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ Format """
-        return filter_datum(self.fields, self.REDACTION,
-                            super().format(record), self.SEPARATOR)
+        """
+        Format the log record, obfuscating specified fields.
+
+        :param record: The log record to format.
+        :return: The formatted log record as a string.
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
+
+PII_FIELDS: Tuple[str, ...] = ("name", "email", "phone", "ssn", "password")
 
 
 def get_logger() -> logging.Logger:
-    """ Implementing a logger.
     """
+    Create and configure a logger named 'user_data' to log obfuscated messages.
 
+    :return: Configured logger object.
+    """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    handler = logging.StreamHandler()
-    handler.setFormatter(RedactingFormatter(PII_FIELDS))
-    logger.addHandler(handler)
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=list(PII_FIELDS))
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
     return logger
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """ Implement db conectivity
     """
-    psw = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
-    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
-    conn = mysql.connector.connect(
-        host=host,
-        database=db_name,
+    Connect to the MySQL database using credentials from environment variables.
+
+    :return: MySQLConnection object.
+    """
+    username = environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = environ.get('PERSONAL_DATA_DB_PASSWORD', '')
+    host = environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = environ.get('PERSONAL_DATA_DB_NAME')
+
+    connect = mysql.connector.connection.MySQLConnection(
         user=username,
-        password=psw)
-    return conn
+        password=password,
+        host=host,
+        database=db_name)
+
+    return connect
 
 
-def main() -> None:
-    """ Implement a main function
+def main():
+    """
+    Main function that retrieves all rows from the users table and
+    logs each row with sensitive fields obfuscated.
     """
     db = get_db()
     cursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
+    field_names = [i[0] for i in cursor.description]
+
+    logger = get_logger()
+
     for row in cursor:
-        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
-            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
-            f"last_login={row[6]}; user_agent={row[7]};"
-        print(message)
+        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
+        logger.info(str_row.strip())
+
     cursor.close()
     db.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
