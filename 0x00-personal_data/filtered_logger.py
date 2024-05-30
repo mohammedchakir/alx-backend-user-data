c7019903b184d21568dfe35267983f528cb5081c
mohammedchakir <zerroukimedchakir@gmail.com>
2024-05-30 16:46:43 +0100

    add

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,82 +1,87 @@
 #!/usr/bin/env python3
-""" doc doc doc """
-import re
-from typing import List
+"""
+Personal data
+"""
+
+
 import logging
 import os
+import re
+from typing import List
 import mysql.connector
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
-def filter_datum(
-    fields: List[str], redaction: str, message: str, separator: str
-) -> str:
-    """doc doc doc"""
-    for field in fields:
-        regex = f"{field}=[^{separator}]*"
-        message = re.sub(regex, f"{field}={redaction}", message)
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Replacing """
+    for f in fields:
+        message = re.sub(rf"{f}=(.*?)\{separator}",
+                         f'{f}={redaction}{separator}', message)
     return message
 
 
 class RedactingFormatter(logging.Formatter):
-    """doc doc doc"""
+    """ RedactingFormatter class. """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """doc doc doc"""
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        """ Init """
         self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
-        """doc doc doc"""
-        org = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, org, self.SEPARATOR)
-
-
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+        """ Format """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
-    """doc doc doc"""
-    log = logging.getLogger("user_data")
-    log.setLevel(logging.INFO)
-    log.propagate = False
-    sh = logging.StreamHandler()
-    sh.setFormatter(RedactingFormatter(PII_FIELDS))
-    log.addHandler(sh)
-    return log
+    """ Implementing a logger.
+    """
+
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """doc doc doc"""
-    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-
-    return mysql.connector.connect(
-        user=username, password=password, host=host, database=db_name
-    )
+    """ Implement db conectivity
+    """
+    psw = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
+    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+    conn = mysql.connector.connect(
+        host=host,
+        database=db_name,
+        user=username,
+        password=psw)
+    return conn
 
 
 def main() -> None:
-    """doc doc doc"""
+    """ Implement a main function
+    """
     db = get_db()
     cursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
-    log = get_logger()
     for row in cursor:
-        data = []
-        for desc, value in zip(cursor.description, row):
-            pair = f"{desc[0]}={str(value)}"
-            data.append(pair)
-        row_str = "; ".join(data)
-        log.info(row_str)
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
+            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
+            f"last_login={row[6]}; user_agent={row[7]};"
+        print(message)
     cursor.close()
     db.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
