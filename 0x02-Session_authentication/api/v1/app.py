17e64b10959de40452815dfb73cb34c5e333f596
mohammedchakir <zerroukimedchakir@gmail.com>
2024-06-03 21:28:22 +0100

    push

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,6 +6,7 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
+from api.v1.auth.auth import Auth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -33,14 +34,21 @@ def before_request():
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
+    if request.path in excluded_paths:
+        return
+
     if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None and not isinstance(
-            auth, SessionAuth):
+        auth_header = auth.authorization_header(request)
+        session_cookie = auth.session_cookie(request)
+        if auth_header is None and session_cookie is None:
             abort(401)
-        request.current_user = auth.current_user(request)
-        if request.current_user is None:
+        if auth.current_user(request) is None:
             abort(403)
 
 
