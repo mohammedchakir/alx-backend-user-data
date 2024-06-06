f41f7f69cbea4f63669d383d9ac93e90ba2a2f57
mohammedchakir <zerroukimedchakir@gmail.com>
2024-06-06 03:20:29 +0100

    add

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -3,7 +3,7 @@
 This module contains the routes for session authentication.
 """
 
-from flask import Blueprint, request, jsonify, abort
+from flask import request, jsonify, abort
 from models.user import User
 from os import getenv
 from api.v1.views import app_views
@@ -56,7 +56,6 @@ def auth_session_logout():
     """
     from api.v1.app import auth
 
-    if not auth.destroy_session(request):
-        abort(404)
-
-    return jsonify({}), 200
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
