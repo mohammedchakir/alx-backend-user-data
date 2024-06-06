88b19d64088f1a92e60deb3cf3567d3b50046722
mohammedchakir <zerroukimedchakir@gmail.com>
2024-06-06 08:39:34 +0100

    add

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,56 +1,44 @@
 #!/usr/bin/env python3
 """
-This module contains the routes for session authentication.
+Session authentication views.
 """
-
-from flask import request, jsonify, abort
-from models.user import User
-import os
+from flask import jsonify, request, abort, make_response
 from api.v1.views import app_views
+from models.user import User
+from os import getenv
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def auth_session_login():
-    """
-    Handle the login route for session authentication.
-
-    Returns:
-        A JSON response containing the user information if login is successful.
-        Otherwise, it returns an error message with appropriate status code.
-    """
-    email = request.form.get('email')
-    password = request.form.get('password')
-    if email is None or email == '':
-        return jsonify({"error": "email missing"}), 400
-    if password is None or password == '':
-        return jsonify({"error": "password missing"}), 400
-    users = User.search({"email": email})
-    if not users or users == []:
-        return jsonify({"error": "no user found for this email"}), 404
-    for user in users:
-        if user.is_valid_password(password):
-            from api.v1.app import auth
-            session_id = auth.create_session(user.id)
-            response = jsonify(user.to_json())
-            session_name = os.getenv('SESSION_NAME')
-            response.set_cookie(session_name, session_id)
-            return response
-
-    return jsonify({"error": "wrong password"}), 401
-
-
-@app_views.route('/auth_session/logout', methods=['DELETE'],
-                 strict_slashes=False)
-def auth_session_logout():
-    """
-    Handle the logout route for session authentication.
-
-    Returns:
-        A JSON response with an empty dictionary if logout is successful.
-        Otherwise, it returns a 404 error.
+def login():
+    """ POST /api/v1/auth_session/login
+    Return:
+      - User object JSON represented
+      - 400 if email or password is missing
+      - 404 if no user found with email
+      - 401 if the password is wrong
     """
     from api.v1.app import auth
 
-    if auth.destroy_session(request):
-        return jsonify({}), 200
-    abort(404)
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({"email": email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    user = user[0]
+
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    session_id = auth.create_session(user.id)
+    user_dict = user.to_dict()
+    response = make_response(jsonify(user_dict))
+    session_name = getenv("SESSION_NAME")
+    response.set_cookie(session_name, session_id)
+
+    return response
