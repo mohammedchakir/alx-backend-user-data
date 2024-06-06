08025988b3cb9f59fd39cbc294e98bfae195c09f
mohammedchakir <zerroukimedchakir@gmail.com>
2024-06-06 15:41:18 +0100

    add

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -39,6 +39,18 @@ def view_one_user(user_id: str = None) -> str:
     return jsonify(user.to_json())
 
 
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def view_me_user() -> str:
+    """ GET /api/v1/users/me
+    Return:
+      - Authenticated User object JSON represented
+      - 404 if the User is not authenticated
+    """
+    if request.current_user is None:
+        abort(404)
+    return jsonify(request.current_user.to_json())
+
+
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
     """ DELETE /api/v1/users/:id
