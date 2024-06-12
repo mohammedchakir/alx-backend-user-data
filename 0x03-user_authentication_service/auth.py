57c1db43612d525f489c6934de7c1b81c95e980a
mohammedchakir <zerroukimedchakir@gmail.com>
2024-06-12 12:19:00 +0100

    add

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
 
+"""
+Auth module
+"""
+
+import bcrypt
+from typing import Union
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password using bcrypt.
+    Args:
+        password (str): The password to hash.
+    Returns:
+        bytes: The salted hash of the password.
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
