b25f1f8acb7747792352fd591bb8512871d31721
mohammedchakir <zerroukimedchakir@gmail.com>
2024-06-12 12:11:49 +0100

    add

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,7 @@
 User model for the users table in the database.
 """
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -15,8 +15,8 @@ class User(Base):
     """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(250), nullable=False, unique=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
